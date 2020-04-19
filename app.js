cc6788ea4edc2d22c16147b67e551a8803012f21
Timothy <timothymayor24@gmail.com>
2020-04-19 21:16:56 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+function divisible(num){
+    if (num % 2 ===0 && num % 3 === 0 && num % 5 ===0){
+        return "yu-gi-oh"
+    }
+    else if (num % 2 ===0 && num % 3 === 0){
+        return "yu-gi"
+    } 
+     else if (num % 2 ===0 && num % 5 === 0){
+        return "yu-oh"
+    }
+    else if (num % 3 ===0 && num % 5 === 0){
+        return "gi-oh"
+    }
+    else if (num % 2 === 0){
+        return "yu"
+    }
+    else if (num % 3 === 0){
+        return "gi"
+    }
+    else if (num % 5 === 0){
+        return "oh"
+    }
+    else{
+        return num
+    }
+}
+
+function calcTotalNum(val){
+    let arr = [];
+    for (let i = 0; i<=val; i++){
+        arr.push(divisible(i))
+    }
+    console.log(arr)
+}
+
+calcTotalNum(100)
+calcTotalNum(10)
